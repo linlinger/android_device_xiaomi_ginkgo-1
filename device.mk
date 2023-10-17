fb100785b8bdbea7bee2b9ff0bbb2581fc06fe27
linlinger <32541118+linlinger@users.noreply.github.com>
2023-10-17 15:29:23 +0800

    Set up retrofit partition

--- device.mk
+++ device.mk
@@ -142,6 +142,16 @@ PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
     android.hardware.drm@1.3.vendor
 
+# Dynamic Partitions
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom_ramdisk
+LOCAL_MODULE_STEM  := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.ginkgo
