a4cb2807d3739c90372c176f98795cced6de99c8
linlinger <32541118+linlinger@users.noreply.github.com>
2024-01-02 11:58:22 +0800

    getting ready for pe

--- aosp_ginkgo.mk
+++ aosp_ginkgo.mk
@@ -29,9 +29,6 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Inherit Some elixir stuff
-$(call inherit-product, device/xiaomi/ginkgo/elixir.mk)
-
 # Device identifier
 PRODUCT_NAME := aosp_ginkgo
 PRODUCT_DEVICE := ginkgo
