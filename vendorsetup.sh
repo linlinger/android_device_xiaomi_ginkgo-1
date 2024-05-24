fb5f08b32842ecbf3c0fa26650366e0ea0788de0
linlinger <32541118+linlinger@users.noreply.github.com>
2024-05-24 19:38:00 +0800

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+# rm -rf vendor/xiaomi/ginkgo
+# rm -rf kernel/xiaomi/ginkgo
+# rm -rf hardware/xiaomi/aidl/power-libperfmgr
+
+# git clone --depth=1 https://github.com/Evolution-X-Devices/vendor_xiaomi_ginkgo.git -b udc vendor/xiaomi/ginkgo
+# git clone --depth=1 https://github.com/linlinger/kernel_xiaomi_ginkgo -b fourteen_dynamic_erofs
